5098e7685fa3ef35a768ea6c7b611bbffa5adf09
Richard Lanham <richard@RichardM1ProMac.local>
2023-02-22 18:18:16 -0500

    fix ignore

--- config/plugins.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-module.exports = ({ env }) => ({
-  'users-permissions': {
-    config: {
-      jwtSecret: env('JWT_3233') || crypto.randomBytes(16).toString('base64'),
-    },
-  },
-});
